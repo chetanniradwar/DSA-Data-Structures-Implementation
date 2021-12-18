71d144ae9f6ad4e505c8f987781ccd79a1ca3e67
chetannirdwar <62559514+chetannirdwar@users.noreply.github.com>
2021-12-18 18:52:27 +0530

    hashing collision avoidance Direct chaining (insert ,search)

---
+++ HASHING/Collision Resolution Techniques/Direct Chainning/DirectChaining.java
@@ -0,0 +1,59 @@
+
+import java.util.LinkedList;
+public class DirectChaining {
+ 
+int arr[] = new int[10];
+LinkedList<String> hashTable[];
+   public DirectChaining(){
+    hashTable = new LinkedList[100];
+
+   }
+   public int hashFunction(String x , int M)
+   {
+    char ch[];
+    ch = x.toCharArray();
+  
+
+    int i, sum;
+    for (sum=0, i=0; i < x.length(); i++) {
+      sum += ch[i];
+    }
+    return sum % M;
+   }
+
+   public void insert(String key)
+   {
+    int index = hashFunction(key, hashTable.length);
+    if(hashTable[index]==null)  
+    {
+        LinkedList<String> l = new LinkedList<String>();
+        l.add(key);
+        hashTable[index]=l;
+    } 
+   
+    else
+    {
+        hashTable[index].add(key);
+    }
+   }
+
+   public int search(String key)
+   {
+            int index =hashFunction(key, hashTable.length);
+            if(hashTable[index]==null)
+            {
+                return -1;
+            }
+            else{
+                LinkedList<String> tmp = hashTable[index];
+                if(tmp.contains(key))
+                {
+                    return index;
+                }
+                else 
+                return -1;
+            }
+   }
+   
+  
+}
---
+++ HASHING/Collision Resolution Techniques/Direct Chainning/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String args[])
+    {
+       DirectChaining dc = new DirectChaining();
+       dc.insert("chetan");
+       dc.insert("niradwar");
+       dc.insert("atnhce");
+       System.out.println(dc.search("atnhce"));
+    }
+}
