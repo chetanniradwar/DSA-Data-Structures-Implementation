e968edba0a5dd36b6f31178038ebe5e6256255aa
chetannirdwar <62559514+chetannirdwar@users.noreply.github.com>
2021-12-21 19:59:50 +0530

    implemented quadratic probing

---
+++ HASHING/Collision Resolution Techniques/Open Addressing/Quadratic Probing/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+    QuadraticProbing qp = new QuadraticProbing();
+        // qp.insert("chetan");
+        // qp.insert("niradwar");
+        // qp.insert("atnhce");
+        // qp.insert("apple");
+        // qp.insert("ball");
+        qp.insert("cat");
+        // qp.insert("maple");
+        // qp.insert("box");
+        qp.insert("tac");
+        qp.insert("cta");
+        qp.insert("atc");
+
+        qp.delete("sfd");
+
+
+     //    System.out.println(dc.search("atnhce"));
+        // qp.display();
+
+        // qp.search("chetan");
+        // qp.search("atnhce");
+        //  qp.search("buzz");
+        // qp.search("tac");
+
+        qp.delete("tac");
+        // qp.delete("chetan");
+        // qp.delete("atnhce");
+     qp.delete("atc");
+        // qp.display();
+
+    }
+}
---
+++ HASHING/Collision Resolution Techniques/Open Addressing/Quadratic Probing/QuadraticProbing.java
@@ -0,0 +1,119 @@
+
+
+public class QuadraticProbing {
+    String hashTable[] ;
+    int filledcells;
+    public QuadraticProbing()
+    {
+        hashTable =new String[10];
+        filledcells= 0;
+    }
+
+    public void reHashtable()
+    {
+       
+                String newhashTable[] = new String[2*hashTable.length];
+                for(int i = 0 ;i<  hashTable.length ; i++)
+                {
+                    newhashTable[i] = hashTable[i];
+                }
+                hashTable = newhashTable ;
+
+        
+    }
+
+
+    public double calcLoadFactor()
+    {
+            double loadFactor = filledcells * 1.0/hashTable.length;
+            return loadFactor;
+    }
+
+    public void insert(String key)
+    {   
+        
+        int counter = 0;
+        int index =hashFunction(key , hashTable.length);
+        for(int i = index ;; i=(index+(counter*counter))%hashTable.length)
+        {   
+            // System.out.println("in a loop");
+            if(hashTable[i]==null)
+            { 
+            hashTable[i] = key ;
+            filledcells++;
+            if(calcLoadFactor()>0.75)
+            reHashtable();
+            break;
+            }
+            counter++;
+            
+           
+        }
+       
+
+
+    }
+
+    public int hashFunction(String key , int M)
+    {
+        char arr[] = key.toCharArray();
+        int asciisum = 0;
+        for(int i = 0 ; i< arr.length ; i++)
+        {
+            asciisum = asciisum + arr[i] ;
+        }
+        return asciisum % M ;
+        
+    }
+
+
+    public void display()
+   {    if(hashTable == null)
+    {
+        System.out.println("HashTable does not exist") ;
+        return;
+    }
+       for(int i = 0 ; i< hashTable.length; i++)
+       {
+           System.out.println(i +" -> " + hashTable[i]);
+       }
+   }
+
+
+   public void search(String key)
+    {
+        int index =hashFunction(key, hashTable.length);
+        
+        for(int i = index ;i<index+hashTable.length;i++ )
+        {
+           
+            int newindex = i% hashTable.length;
+            if(hashTable[newindex] != null &&hashTable[newindex].equals(key))
+            {
+                System.out.println("This key is present on index :"+ newindex) ;
+                return;
+            }
+        }
+        System.out.println("not found") ;
+
+        
+    }
+
+    public void delete(String key)
+    {
+        int index =hashFunction(key, hashTable.length);
+        
+        for(int i = index ;i<index+hashTable.length;i++ )
+        {
+            int newindex = i% hashTable.length;
+            if(hashTable[newindex] != null && hashTable[newindex].equals(key))
+            {
+                System.out.println("This key is present on index :"+ newindex) ;
+                hashTable[newindex] = null;
+                System.out.println("Successfully deleted") ;
+                return;
+            }
+        }
+        System.out.println("The key is not present in Hashtable") ;
+    }
+}
