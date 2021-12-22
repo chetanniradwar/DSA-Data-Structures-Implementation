79d472a0c24b12e2e9b53a688f999ec0ef7b58d2
chetannirdwar <62559514+chetannirdwar@users.noreply.github.com>
2021-12-22 22:50:24 +0530

    Added Double hashing

---
+++ HASHING/Collision Resolution Techniques/Open Addressing/Double Hashing/DoubleHashing.java
@@ -0,0 +1,135 @@
+public class DoubleHashing {
+    String[] hashTable; 
+    int filledCells ;
+    public DoubleHashing()
+    {
+        hashTable = new String[10] ;
+        filledCells = 0 ;
+    }
+
+    public int hashFunction1(String key, int M)
+    {
+        char arr[] = key.toCharArray();
+        int  sum = 0 ;
+        for(char x: arr)
+        {
+                sum = sum + x;
+        }
+        return sum % M;
+    }
+
+    public int hashFunction2(String key , int M)
+    {
+        char arr[] = key.toCharArray();
+        int  sum = 0 ;
+        for(char x: arr)
+        {
+         sum = sum +(2*x);
+        }
+        System.out.println("hashFunction 2 sum "+sum);
+        return sum % M;
+    }
+
+    public void insert(String key )
+    {
+        int index1 = hashFunction1(key , hashTable.length);
+        
+        if(hashTable[index1]==null)
+        {
+            hashTable[index1] = key ;
+        }
+        else{
+            int index2 = hashFunction2(key, hashTable.length);
+            int counter =  1;
+            int newIndex;
+            while(true)
+            {
+
+                newIndex = (index1 +(counter * index2))%hashTable.length ;
+            if(hashTable[newIndex]==null)
+            {
+                hashTable[newIndex] = key;
+                if(calcLoadFactor()>=0.75)
+                reHashtable();
+                break;
+            }
+            else
+            {
+                counter++;
+            }
+        }
+        }
+    }
+
+
+    public void display()
+    {    if(hashTable == null)
+     {
+         System.out.println("HashTable does not exist") ;
+         return;
+     }
+        for(int i = 0 ; i< hashTable.length; i++)
+        {
+            System.out.println(i +" -> " + hashTable[i]);
+        }
+    }
+ 
+ 
+    public void search(String key)
+     {
+         int index =hashFunction1(key, hashTable.length);
+         
+         for(int i = index ;i<index+hashTable.length;i++ )
+         {
+            
+             int newindex = i% hashTable.length;
+             if(hashTable[newindex] != null &&hashTable[newindex].equals(key))
+             {
+                 System.out.println("This key is present on index :"+ newindex) ;
+                 return;
+             }
+         }
+         System.out.println("not found") ;
+ 
+         
+     }
+ 
+     public void delete(String key)
+     {
+         int index =hashFunction1(key, hashTable.length);
+         
+         for(int i = index ;i<index+hashTable.length;i++ )
+         {
+             int newindex = i% hashTable.length;
+             if(hashTable[newindex] != null && hashTable[newindex].equals(key))
+             {
+                 System.out.println("This key is present on index :"+ newindex) ;
+                 hashTable[newindex] = null;
+                 System.out.println("Successfully deleted") ;
+                 return;
+             }
+         }
+         System.out.println("The key is not present in Hashtable") ;
+     }
+     public void reHashtable()
+     {
+        
+                 String newhashTable[] = new String[2*hashTable.length];
+                 for(int i = 0 ;i<  hashTable.length ; i++)
+                 {
+                     newhashTable[i] = hashTable[i];
+                 }
+                 hashTable = newhashTable ;
+ 
+         
+     }
+ 
+ 
+     public double calcLoadFactor()
+     {
+             double loadFactor = filledCells * 1.0/hashTable.length;
+             return loadFactor;
+     }
+ 
+ }
+
---
+++ HASHING/Collision Resolution Techniques/Open Addressing/Double Hashing/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        DoubleHashing dh = new DoubleHashing();
+        // dh.insert("The");
+        // dh.insert("quick");
+        // dh.insert("brown");
+        // dh.insert("fox");
+        // dh.insert("over");
+        // dh.insert("lazy");
+         dh.insert("cat");
+        dh.insert("tac");
+        dh.insert("act");
+        dh.insert("atc");
+        dh.insert("cta");
+        // dh.insert("lazy");
+
+        // dh.delete("sfd");
+
+
+     //    System.out.println(dc.search("atnhce"));
+         dh.display();
+
+        // dh.search("chetan");
+        // dh.search("atnhce");
+        //  dh.search("buzz");
+        // dh.search("tac");
+
+        // dh.delete("tac");
+        // dh.delete("chetan");
+        // dh.delete("atnhce");
+    //  dh.delete("atc");
+        
+    }
+}
