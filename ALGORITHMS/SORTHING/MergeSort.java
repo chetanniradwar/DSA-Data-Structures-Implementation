e0b3c1b71ba4730f99991abb7b6713beb63b2c37
chetannirdwar <62559514+chetannirdwar@users.noreply.github.com>
2021-12-26 13:22:08 +0530

    Removed redundency code

--- ALGORITHMS/SORTHING/MergeSort.java
+++ ALGORITHMS/SORTHING/MergeSort.java
@@ -2,45 +2,11 @@ import java.util.Arrays;
 
 public class MergeSort {
     public static void main(String[] args) {
+
         int arr[] = new int[] {9,8,7,6,5,4,3,2,1};
-        int arr1[] = new int[arr.length / 2];
-        int arr2[] = new int[(arr.length + 1) / 2];
-        for (int i = 0; i < arr.length; i++) {
-            if (i < arr.length / 2)
-                arr1[i] = arr[i];
-            else
-                arr2[i - arr.length / 2] = arr[i];
-        }
-        arr1 = mergeSort(arr1);
-        arr2 = mergeSort(arr2);
-        for (int i = 0; i < arr.length; i++) {
-            int minimum = Integer.MAX_VALUE;
-                int minindex = -1;
-            for (int j = 0; j < arr.length; j++) {
-                if (j < arr.length / 2) {
-
-                    if (arr1[j] < minimum ) {
-                        minimum = arr1[j];
-                        minindex = j;
-
-                    }
-                } else {
-
-                    if (arr2[j - arr.length / 2] < minimum) {
-                        minimum = arr2[j - arr.length / 2];
-                        minindex = j;
-                    }
-                }
-            }
-                arr[i] = minimum;
-                if(minindex<arr.length/2)
-                arr1[minindex] = Integer.MAX_VALUE;
-                else
-                arr2[minindex-arr.length/2]= Integer.MAX_VALUE;
-            minimum = Integer.MAX_VALUE;
-
-        }
+         arr =mergeSort(arr);
         System.out.print(Arrays.toString(arr));
+        
 
     }
 
